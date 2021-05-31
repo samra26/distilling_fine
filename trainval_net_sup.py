2ed7e6cb31b97c60a10d7d2880402e028ca22eb2
samra26 <75056386+samra26@users.noreply.github.com>
2021-05-31 16:11:23 +0500

    Update trainval_net_sup.py

--- trainval_net_sup.py
+++ trainval_net_sup.py
@@ -366,10 +366,10 @@ if __name__ == '__main__':
             data = next(data_iter)
             #pytorch1
             with torch.no_grad():
-                im_data.data.resize_(data[0].size()).copy_(data[0])
-                im_info.data.resize_(data[1].size()).copy_(data[1])
-                gt_boxes.data.resize_(data[2].size()).copy_(data[2])
-                num_boxes.data.resize_(data[3].size()).copy_(data[3])
+                im_data.resize_(data[0].size()).copy_(data[0])
+                im_info.resize_(data[1].size()).copy_(data[1])
+                gt_boxes.resize_(data[2].size()).copy_(data[2])
+                num_boxes.resize_(data[3].size()).copy_(data[3])
 
             fasterRCNN.zero_grad()
             rois, cls_prob, bbox_pred, \
